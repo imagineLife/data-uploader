da127551b367a9961e126db0f90f7568d6fd63d8
Eric Laursen <ericjlaursen@gmail.com>
2020-08-21 07:56:32 -0400

    root renderer

---
+++ renderer.js
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+import { render } from 'react-dom';
+import App from './src/App'
+
+console.log('---- RENDERER.js ----');
+
+// render(<App />, document.getElementById('app'));
