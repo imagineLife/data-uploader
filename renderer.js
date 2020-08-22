8e8df97d9a9588aed63c29cee22c8a4a132d1bb2
Eric Laursen <ericjlaursen@gmail.com>
2020-08-22 09:33:01 -0400

    renders react app

--- renderer.js
+++ renderer.js
@@ -2,6 +2,4 @@ import React, { useState } from 'react';
 import { render } from 'react-dom';
 import App from './src/App'
 
-console.log('---- RENDERER.js ----');
-
-// render(<App />, document.getElementById('app'));
+render(<App />, document.getElementById('app'));
