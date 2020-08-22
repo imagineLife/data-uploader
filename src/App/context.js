d64416d68b114f564ad6e8a868c0c79508fd254c
Eric Laursen <ericjlaursen@gmail.com>
2020-08-22 19:42:30 -0400

    parse data

--- src/App/context.js
+++ src/App/context.js
@@ -15,7 +15,38 @@ const AppProvider = ({children}) => {
 			const readFileFn = async () => {
 				try{
 					let fileData = await fs.readFile(uploadedFilePath, {encoding: 'utf-8'});
-					setFileData(fileData)
+					let lines = fileData.split('\n')
+					let res = {}
+					res.headers = lines[0].split(',')
+					let colCount = res.headers.length;
+					res.data = lines.reduce((resArr, curRowStr, rowIdx) => {
+						if(rowIdx === 0) return []
+							let hasQuote = curRowStr.split("\"").length > 1
+							if(!hasQuote){
+								return [...resArr, curRowStr.split(',')]
+							}else{
+								let splitAtQuote = curRowStr.split("\"")
+								let splitAtCommas = curRowStr.split(',')
+								return [...resArr, splitAtCommas.reduce((resArr, curItm, idx) => {
+									if(!curItm.includes("\"")){
+										return [...resArr, curItm]
+									}else{
+										if(curItm.indexOf("\"") !== 0){
+											return [...resArr];
+										}
+										
+										let fixedCurString = curItm.replace("\"","")
+										let nextStr = splitAtCommas[idx + 1]
+										let cleanNextStr = nextStr.replace("\"","")
+										let finalStr = `${fixedCurString},${cleanNextStr}`
+										return [...resArr, finalStr]
+										
+									}
+								},[])]
+							}
+					},[])
+
+					setFileData(res)
 				}catch(e){
 					console.log('readFileFn e')
 					console.log(e)
