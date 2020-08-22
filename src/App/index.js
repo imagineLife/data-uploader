35f49d03fa79cfc47df0076bfb32a8b9e1af339d
Eric Laursen <ericjlaursen@gmail.com>
2020-08-22 09:33:12 -0400

    text output

--- src/App/index.js
+++ src/App/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
 
-const App = () => (<div>App Here</div>)
-console.log('----/sr/App/index.js----');
+const App = () => (<div>/src/App/index.js Here</div>)
 export default App;
